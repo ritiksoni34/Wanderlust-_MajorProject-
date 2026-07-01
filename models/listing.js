4cc8946e9ef273b9d4716b9f05cff937fe669efd
Ritik soni <ritiksoni7008@gmail.com>
2026-07-01 18:32:29 +0530

    change image default

--- models/listing.js
+++ models/listing.js
@@ -8,10 +8,13 @@ const listingSchema = new Schema({
         type:String
     },
     description:String,
-    img:{
-        type:String,
-        default:"https://www.travelandleisure.com/thmb/x8L30CdZCZAe8a1LmIOnJcBJXhQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-USE-header-exterior-sign-flat-iron-hotel-asheville-FLATIRONASH0524-28ee8bc1cfed48ae80f2db1ccf02a7fd.jpg",
-        set:(v)=>v==="" ? "https://www.travelandleisure.com/thmb/x8L30CdZCZAe8a1LmIOnJcBJXhQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-USE-header-exterior-sign-flat-iron-hotel-asheville-FLATIRONASH0524-28ee8bc1cfed48ae80f2db1ccf02a7fd.jpg" : v
+    image:{
+        filename:{type:String},
+        url:{
+            type: String,
+            default:"https://www.travelandleisure.com/thmb/x8L30CdZCZAe8a1LmIOnJcBJXhQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-USE-header-exterior-sign-flat-iron-hotel-asheville-FLATIRONASH0524-28ee8bc1cfed48ae80f2db1ccf02a7fd.jpg",
+            set:(v)=>v==="" ? "https://www.travelandleisure.com/thmb/x8L30CdZCZAe8a1LmIOnJcBJXhQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-USE-header-exterior-sign-flat-iron-hotel-asheville-FLATIRONASH0524-28ee8bc1cfed48ae80f2db1ccf02a7fd.jpg" : v
+        }
     },
     price:Number,
     location:String,
